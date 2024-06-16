cbc71010ad2fa6d746174c3dc6b6ba7c1f218fc0
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2024-06-16 08:37:20 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili/diy-part2.sh
+++ JDCloud-AX6000-Baili/diy-part2.sh
@@ -10,20 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-##-----------------Delete DDNS's examples-----------------
-sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
-##-----------------Manually set CPU frequency for MT7986A-----------------
-sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
-
 #  steven ->
 # https://github.com/stupidloud/nanopi-openwrt/blob/master/scripts/merge_packages.sh
 function merge_package(){
@@ -34,16 +20,9 @@ function merge_package(){
     mv $2 package/custom/
     rm -rf $repo
 }
-
 rm -rf package/custom; mkdir package/custom
 
-rm -rf package/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openclash
-merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
 
-rm -rf package/luci-app-timecontrol
-rm -rf feeds/luci/applications/luci-app-timecontrol
-merge_package https://github.com/Lienol/openwrt-package.git openwrt-package/luci-app-timecontrol
 
 rm -rf packages/net/zerotier
 rm -rf feeds/packages/net/zerotier
@@ -56,5 +35,34 @@ git clone --depth 1 https://github.com/sbwml/openwrt-alist.git package/custom/lu
 rm -rf package/luci-app-wechatpush
 rm -rf feeds/luci/applications/luci-app-wechatpush
 git clone --depth 1 https://github.com/tty228/luci-app-wechatpush.git package/custom/luci-app-wechatpush
+
+rm -rf package/luci-app-timecontrol
+rm -rf feeds/luci/applications/luci-app-timecontrol
+merge_package https://github.com/Lienol/openwrt-package.git openwrt-package/luci-app-timecontrol
+
+rm -rf package/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+# steven <-
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+##-----------------Add OpenClash dev core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+# steven for new package version
+#- mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+#- mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+mkdir -p package/custom/OpenClash/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash package/custom/OpenClash/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+##-----------------Delete DDNS's examples-----------------
+sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
+##-----------------Manually set CPU frequency for MT7986A-----------------
+sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
+
+
 
 
