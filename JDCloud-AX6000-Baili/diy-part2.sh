cb1a4982cca33fd0c687d70cd0f87a5621269256
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2024-06-16 10:24:35 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili/diy-part2.sh
+++ JDCloud-AX6000-Baili/diy-part2.sh
@@ -47,17 +47,28 @@ merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclas
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-# steven for new package version
-#- mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-#- mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-mkdir -p package/custom/OpenClash/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash package/custom/OpenClash/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+# 预置openclash内核
+mkdir -p files/etc/openclash/core
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat"
+GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat"
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+# 给内核权限
+chmod +x files/etc/openclash/core/clash*
+
+##-----------------Add OpenClash dev core------------------
+#curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+#chmod +x /tmp/clash >/dev/null 2>&1
+## steven for new package version
+##- mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+##- mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+#mkdir -p package/custom/OpenClash/luci-app-openclash/root/etc/openclash/core
+#mv /tmp/clash package/custom/OpenClash/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 ##-----------------Manually set CPU frequency for MT7986A-----------------
