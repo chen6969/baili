bd72fc0ddbe98c107a4c8642475027bc9d2213b6
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2024-06-21 20:43:22 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili/diy-part2.sh
+++ JDCloud-AX6000-Baili/diy-part2.sh
@@ -36,8 +36,8 @@ rm -rf feeds/packages/net/hysteria
 mv immortalwrt_pkg/net/hysteria feeds/packages/net/hysteria
 rm -rf feeds/packages/net/haproxy
 mv immortalwrt_pkg/net/haproxy feeds/packages/net/haproxy
-rm -rf feeds/packages/net/zerotier
-mv immortalwrt_pkg/net/zerotier feeds/packages/net/zerotier
+#rm -rf feeds/packages/net/zerotier
+#mv immortalwrt_pkg/net/zerotier feeds/packages/net/zerotier
 rm -rf feeds/packages/net/smartdns
 mv immortalwrt_pkg/net/smartdns feeds/packages/net/smartdns
 rm -rf feeds/packages/lang/golang
