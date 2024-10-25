38a6a64b153bf6d3e64a82a4820372a5d8553cf7
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2024-10-25 22:41:07 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -9,18 +9,117 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+#  steven ->
+# https://github.com/stupidloud/nanopi-openwrt/blob/master/scripts/merge_packages.sh
+# eg merge_package https://github.com/immortalwrt/packages.git packages/net/zerotier
+#   merge_package 仓库路径.git  仓库名/目录1/目录2/ .../目标目录
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+rm -rf package/custom; 
+mkdir package/custom
+# <----
+
+
+mkdir immortalwrt_pkg
+git clone --depth 1 https://github.com/immortalwrt/packages.git immortalwrt_pkg
+
+rm -rf feeds/packages/net/xray-core
+mv immortalwrt_pkg/net/xray-core feeds/packages/net/xray-core
+
+rm -rf feeds/packages/net/hysteria
+mv immortalwrt_pkg/net/hysteria feeds/packages/net/hysteria
+
+rm -rf feeds/packages/net/haproxy
+mv immortalwrt_pkg/net/haproxy feeds/packages/net/haproxy
+
+rm -rf feeds/packages/net/sing-box
+mv immortalwrt_pkg/net/sing-box feeds/packages/net/sing-box
+
+rm -rf feeds/packages/net/zerotier
+mv immortalwrt_pkg/net/zerotier feeds/packages/net/zerotier
+
+# rm -rf feeds/packages/net/tailscale
+# mv immortalwrt_pkg/net/tailscale feeds/packages/net/tailscale
+# sed -i "7s/nftables/iptables/g" feeds/packages/net/tailscale/files/tailscale.conf
+
+# rm -rf feeds/packages/net/smartdns
+# mv immortalwrt_pkg/net/smartdns feeds/packages/net/smartdns
+
+rm -rf feeds/packages/net/msd_lite
+mv immortalwrt_pkg/net/msd_lite feeds/packages/net/msd_lite
+
+rm -rf feeds/packages/lang/golang
+mv immortalwrt_pkg/lang/golang feeds/packages/lang/golang
+
+rm -rf immortalwrt_pkg
+
+
+
+# New version golang
+#rm -rf feeds/packages/lang/golang
+#rm -rf packages/lang/golang
+#git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#merge_package https://github.com/immortalwrt/packages.git packages/lang/golang
+
+# alist
+sudo apt install libfuse-dev
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+git clone --depth 1 https://github.com/sbwml/openwrt-alist.git package/custom/luci-app-alist
+
+# passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+merge_package https://github.com/xiaorouji/openwrt-passwall.git openwrt-passwall/luci-app-passwall
+
+# smartdns
+#rm -rf feeds/packages/net/smartdns
+#git clone --depth 1 https://github.com/pymumu/openwrt-smartdns.git package/custom/smartdns
+# rm -rf feeds/luci/applications/luci-app-smartdns
+# git clone --depth 1 https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+
+rm -rf feeds/luci/applications/luci-app-wechatpush
+git clone --depth 1 https://github.com/tty228/luci-app-wechatpush.git package/custom/luci-app-wechatpush
+
+rm -rf feeds/luci/applications/luci-app-timecontrol
+merge_package https://github.com/Lienol/openwrt-package.git openwrt-package/luci-app-timecontrol
+
+rm -rf feeds/luci/applications/luci-app-openclash
+merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+
+# FROS是一款路由器防沉迷系统，支持常见的游戏、视频等APP过滤，如抖音、斗鱼、王者荣耀、和平精英等，并支持 APP使用时长统计，小孩网课必备神器。
+# git clone -b fros-21.02 --depth 1 https://github.com/destan19/fros-packages-immortalwrt-mt798x.git package/custom/fros-packages
+# sed -i "s/\$(CONFIG_TARGET_SUBTARGET)/mt7981/g"  package/custom/fros-packages/fros-apps/Makefile
+
+# 预置openclash内核
+mkdir -p files/etc/openclash/core
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat"
+GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat"
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+# 给内核权限
+chmod +x files/etc/openclash/core/clash*
+
+# steven <-
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Del duplicate packages------------------
 rm -rf feeds/packages/net/open-app-filter
 ##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+#curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+#chmod +x /tmp/clash >/dev/null 2>&1
+#mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+#mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 ##-----------------Manually set CPU frequency for MT7986A-----------------
