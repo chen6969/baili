03e983b4696872b2deb5c906529865eb391e8984
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2024-12-10 17:09:06 +0800

    Add files via upload

---
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part2-openwrt.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+
+# alist
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+git clone --depth 1 https://github.com/sbwml/openwrt-alist.git package/custom/luci-app-alist
+# fix alist build fail issue -> https://github.com/sbwml/luci-app-alist
+sudo -E apt-get -qq install libfuse-dev
+
+#wechatpush
+rm -rf feeds/luci/applications/luci-app-wechatpush
+git clone --depth 1 https://github.com/tty228/luci-app-wechatpush.git package/custom/luci-app-wechatpush
+
+git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo.git package/custom/OpenWrt-mihomo
+
+# use official openclash source
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/custom/luci-app-openclash
+
+
+
+# fix linux kernel 6.6.x udp issue
+# compare files with https://github.com/coolsnowwolf/lede/tree/master/target/linux/generic then del all different files
+#rm -rf target/linux/generic/hack-6.6/600-net-enable-fraglist-GRO-by-default.patch
+#rm -rf target/linux/generic/pending-6.6/680-net-add-TCP-fraglist-GRO-support.patch
+#rm -rf target/linux/generic/pending-6.6/681-net-remove-NETIF_F_GSO_FRAGLIST-from-NETIF_F_GSO_SOF.patch
+#rm -rf target/linux/generic/pending-6.6/684-gso-fix-gso-fraglist-segmentation-after-pull-from-fr.patch
+#rm -rf target/linux/generic/pending-6.6/685-net-gso-fix-tcp-fraglist-segmentation-after-pull-fro.patch
+#rm -rf target/linux/generic/backport-6.6/611-01-v6.11-udp-Allow-GSO-transmit-from-devices-with-no-checksum.patch
+#rm -rf target/linux/generic/backport-6.6/611-02-v6.11-net-Make-USO-depend-on-CSUM-offload.patch
+#rm -rf target/linux/generic/backport-6.6/611-03-v6.11-udp-Fall-back-to-software-USO-if-IPv6-extension-head.patch
+
+# 预置openclash内核
+mkdir -p files/etc/openclash/core
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat"
+GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat"
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+ 给内核权限
+chmod +x files/etc/openclash/core/clash*
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+##-----------------Del duplicate packages------------------
+#rm -rf feeds/packages/net/open-app-filter
+##-----------------Add OpenClash dev core------------------
+#curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+#chmod +x /tmp/clash >/dev/null 2>&1
+#mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+#mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+##-----------------Delete DDNS's examples-----------------
+sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
